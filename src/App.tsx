8941ffcb2e7a5edd29b7df2f40c1bd32af81c23b
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2023-09-20 00:00:07 -0300

    Atualização de libs

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,38 @@ import {
 	StyledEngineProvider,
 	CssBaseline,
 } from '@mui/material';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import theme from './theme';
 import GlobalStyles from './GlobalStyles';
 import Pace from './shared/components/Pace';
 import WhatsAppButton from './logged_out/components/whatsapp-button';
 
 const LoggedInComponent = lazy(() => import('./logged_in/components/Main'));
-
 const LoggedOutComponent = lazy(() => import('./logged_out/components/Main'));
 
 function App() {
 	return (
-		<BrowserRouter>
-			<StyledEngineProvider injectFirst>
-				<ThemeProvider theme={theme}>
-					<CssBaseline />
-					<GlobalStyles />
-					<Pace color={theme.palette.primary.light} />
-					<Suspense fallback={<Fragment />}>
-						<Switch>
-							<Route path="/c">
-								<LoggedInComponent />
-							</Route>
-							<Route>
-								<LoggedOutComponent />
-								<WhatsAppButton phoneNumber="seu-número-de-telefone-aqui" />
-							</Route>
-						</Switch>
-					</Suspense>
-				</ThemeProvider>
-			</StyledEngineProvider>
-		</BrowserRouter>
+		<StyledEngineProvider injectFirst>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<GlobalStyles />
+				<Pace color={theme.palette.primary.light} />
+				<Suspense fallback={<Fragment />}>
+					<Routes>
+						<Route path="/c" element={<LoggedInComponent />} />
+						<Route
+							path="/"
+							element={
+								<Fragment>
+									<LoggedOutComponent />
+									<WhatsAppButton phoneNumber="seu-número-de-telefone-aqui" />
+								</Fragment>
+							}
+						/>
+					</Routes>
+				</Suspense>
+			</ThemeProvider>
+		</StyledEngineProvider>
 	);
 }
 
